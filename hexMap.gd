b2f43e169283adb5609d96c3b9104d0c70679047
rferrell03 <referrelliii@gmail.com>
2026-01-08 20:05:57 -0600

    Hex clicking

--- hexMap.gd
+++ hexMap.gd
@@ -1,6 +1,8 @@
 extends Node
 
 
+@onready var map : TileMapLayer = $TileMapLayer 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass 
@@ -9,3 +11,16 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		#get mouse position, convert to hex
+		var mouse_global : Vector2 = event.position
+		var mouse_local := map.to_local(mouse_global)
+		var hex := map.local_to_map(mouse_local)
+		
+		
+		#set hex to orange for now
+		map.set_cell(hex, 4, Vector2i(1,0), 0 )
+		print("Clicked hex: ", hex)
